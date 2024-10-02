06e3f64dffb9377854c8cd0134107193ecf591fe
ışıl vardarlı <isil.vardarli@ogr.iuc.edu.tr>
2024-10-02 18:56:00 +0300

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by IŞIL VARDARLI on 2.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
