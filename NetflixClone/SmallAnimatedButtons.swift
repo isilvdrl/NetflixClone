2f9cf32f893288e5c9ee206a052d4fa681568d29
ışıl vardarlı <isil.vardarli@ogr.iuc.edu.tr>
2024-10-04 16:20:01 +0300

    my list button

---
+++ NetflixClone/SmallAnimatedButtons.swift
@@ -0,0 +1,18 @@
+//
+//  SmallAnimatedButtons.swift
+//  NetflixClone
+//
+//  Created by IŞIL VARDARLI on 4.10.2024.
+//
+
+import SwiftUI
+
+struct SmallAnimatedButtons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SmallAnimatedButtons()
+}
