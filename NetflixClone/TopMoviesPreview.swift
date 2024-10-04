3f034edfab1142c3b6fa57d7b4cb7c203088cd75
ışıl vardarlı <isil.vardarli@ogr.iuc.edu.tr>
2024-10-04 14:51:58 +0300

    movie preview view

---
+++ NetflixClone/TopMoviesPreview.swift
@@ -0,0 +1,18 @@
+//
+//  TopMoviesPreview.swift
+//  NetflixClone
+//
+//  Created by IŞIL VARDARLI on 4.10.2024.
+//
+
+import SwiftUI
+
+struct TopMoviesPreview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TopMoviesPreview()
+}
