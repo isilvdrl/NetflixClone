1a7628595bfb540d04a8ef23f4287260c7cc5d48
ışıl vardarlı <isil.vardarli@ogr.iuc.edu.tr>
2024-10-06 22:18:32 +0300

    top movies preview view

---
+++ NetflixClone/WhiteButton.swift
@@ -0,0 +1,18 @@
+//
+//  WhiteButton.swift
+//  NetflixClone
+//
+//  Created by IŞIL VARDARLI on 6.10.2024.
+//
+
+import SwiftUI
+
+struct WhiteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WhiteButton()
+}
