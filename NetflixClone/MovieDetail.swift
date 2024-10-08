8b156ac5f6f82434ccec1d5a2b9f0cd7c65de826
ışıl vardarlı <isil.vardarli@ogr.iuc.edu.tr>
2024-10-08 12:59:09 +0300

    Movie Detail Page scheme

---
+++ NetflixClone/MovieDetail.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetail.swift
+//  NetflixClone
+//
+//  Created by IŞIL VARDARLI on 8.10.2024.
+//
+
+import SwiftUI
+
+struct MovieDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetail()
+}
