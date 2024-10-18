4f579b2997cf2a2ed307c184a3d125fe044c5846
ışıl vardarlı <isil.vardarli@ogr.iuc.edu.tr>
2024-10-18 23:42:31 +0300

    Episode view

---
+++ NetflixClone/MovieDetail/EpisodesView.swift
@@ -0,0 +1,18 @@
+//
+//  EpisodesView.swift
+//  NetflixClone
+//
+//  Created by IŞIL VARDARLI on 18.10.2024.
+//
+
+import SwiftUI
+
+struct EpisodesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EpisodesView()
+}
